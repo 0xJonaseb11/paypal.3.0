6a1bdec92b04ec19af29ae11747599dc40629d82
Jaz-3-0 <sebejaz99@gmail.com>
2024-03-28 09:07:29 +0200

    update init deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,26 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+const Main = async () => {
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const Paypal = await hre.ethers.getContractFactory("Paypal");
+  const paypal = await Paypal.deploy();
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  console.log("Deploying Paypal contract...");
 
-  await lock.waitForDeployment();
+  await paypal.deployed();
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log("Paypal contract deployed to:", paypal.address);  
+
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+const runMain = async() => {
+  try {
+    await Main();
+    process.exit(0);
+  } catch (err) {
+    console.log("Error deploying Paypal contract", err);
+    process.exit(1);
+  }  
+}
+
+runMain();
