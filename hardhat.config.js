91feee7142808e154638c2b030fbce7d6254a695
Jaz-3-0 <sebejaz99@gmail.com>
2024-03-29 14:37:28 +0200

    update init configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,19 +2,20 @@ require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 
-const dotenv = require("detenv");
+const dotenv = require("dotenv");
 dotenv.config();
 
 /*
-* This file contains both test and production configuration approaches.
-* testnets
-* mainnets
-*/
+ * This file contains both test and production configuration approaches.
+ * testnets
+ * mainnets
+ */
 
 /** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: {
-    version: "0.8.24",
+  module.exports = {
+     solidity: {
+      version: "0.8.24",
+      defaultNetwork: mumbai,
   },
   networks: {
     hardhat: {},
@@ -22,41 +23,40 @@ module.exports = {
       url: process.env.SEPOLIA,
       accounts: [process.env.PRIVATE_KEY],
       chainId: 11155111,
-
     },
     mumbai: {
       url: process.env.POLYGON_MUMBAI,
-      accounts: [process.env.PRIVATE_KEY],
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
       chainId: 80001,
     },
 
     arbitrum_sepolia: {
       url: process.env.ARBITRUM_SEPOLIA,
-      accounts: [process.env.PRIVATE_KEY],
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
       chainId: 421614,
     },
-    
+
     // Main networks
     mainnet: {
       url: process.env.MAINNET,
-      accounts: [process.env.PRIVATE_KEY],
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
       chainId: 1,
     },
     polygon: {
       url: process.env.POLYGON,
-      accounts: [process.env.PRIVATE_KEY],
-      chainId: '',
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      chainId: "",
     },
     optimism: {
       url: process.env.OPTIMISM,
-      accounts: [process.env.PRIVATE_KEY],
-      chainId: '',
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      chainId: "",
     },
     arbitrum_one: {
       url: process.env.ARBITRUM_ONE,
-      accounts: [process.env.PRIVATE_KEY],
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
       chainId: 42161,
-    }
+    },
   },
 
   settings: {
@@ -65,4 +65,5 @@ module.exports = {
       runs: 200,
     },
   },
-};
+ }
+
