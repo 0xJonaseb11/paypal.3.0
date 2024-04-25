698348e94569aec3438f5083a67fb569cf8c069d
Jaz-3-0 <sebejaz99@gmail.com>
2024-04-25 08:34:25 +0200

    update init get

--- payPalStarter/backend/index.js
+++ payPalStarter/backend/index.js
@@ -12,6 +12,15 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/getNameAndBalance', async(req, res) => {
+
+  const { userAddress } = req.query.user;
+
+  const response = await Moralis.EvmApi.utils.runContractFunction({
+    chain: "0x3881",
+    
+  })
+
+
   return res.status(200).json({});
 });
 
